0015c1e8a2df2a4eb37bc3afb59e356b49c912c6
Anh Ky Nguyen <anh163@gmail.com>
2024-04-01 14:22:05 +0700

    Finished storing and getting quotes from cookies.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import QuotesSection from "./components/QuotesSection";
 import "./index.css";
+import Cookies from "js-cookie";
+import { useEffect } from "react";
+import quotes from "./assets/QuotesData";
 
 function App() {
+  const setDataToCookies = () => {
+    Cookies.set("quotes", JSON.stringify(quotes), { expires: 7 });
+  };
+
+  useEffect(() => {
+    setDataToCookies();
+  }, []);
+
   return (
     <div className="container">
       <Header />
--- src/components/QuotesSection.jsx
+++ src/components/QuotesSection.jsx
@@ -1,22 +1,49 @@
 import styles from "../components/QuotesSection.module.css";
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
 
 function QuotesSection() {
+  const [quote, setQuote] = useState("");
+  const [unusedQuotes, setUnusedQuotes] = useState([]);
+
+  useEffect(() => {
+    const storedQuotes = Cookies.get("quotes");
+    const quotesArray = storedQuotes ? JSON.parse(storedQuotes) : [];
+    if (quotesArray && quotesArray.length > 0) {
+      setQuote(quotesArray[0]);
+      quotesArray.shift();
+    }
+
+    setUnusedQuotes(quotesArray);
+  }, []);
+
+  const handleNextQuote = () => {
+    if (unusedQuotes.length === 0) {
+      setQuote({
+        text: "That's all the jokes for today! Come back another day!",
+      });
+    } else {
+      const shuffledQuotes = unusedQuotes.sort(() => Math.random() - 0.5);
+      const nextQuote = shuffledQuotes[0];
+      setUnusedQuotes((prevUnusedQuotes) => prevUnusedQuotes.slice(1));
+      setQuote(nextQuote);
+    }
+  };
+
   return (
     <div className={styles.section_quotes}>
-      <div className={styles.paragraph}>
-        {`A child asked his father, "How were people born?" So his father said,
-        "Adam and Eve made babies, then their babies became adults and made
-        babies, and so on." The child then went to his mother, asked her the
-        same question and she told him, "We were monkeys then we evolved to
-        become like we are now." The child ran back to his father and said, "You
-        lied to me!" His father replied, "No, your mom was talking about her
-        side of the family."`}
-      </div>
+      <div className={styles.paragraph}>{quote.text}</div>
       <div className={styles.btn_container}>
-        <button className={`${styles.btn} ${styles.btn_blue}`}>
+        <button
+          onClick={handleNextQuote}
+          className={`${styles.btn} ${styles.btn_blue}`}
+        >
           This is funny!
         </button>
-        <button className={`${styles.btn} ${styles.btn_green}`}>
+        <button
+          onClick={handleNextQuote}
+          className={`${styles.btn} ${styles.btn_green}`}
+        >
           This is not funny.
         </button>
       </div>
